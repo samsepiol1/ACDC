4bba00f7ab2d0698a0833bd38ab3ba328ff5cc4a
Lucas Matheus <49918079+samsepiol1@users.noreply.github.com>
2023-11-24 04:14:40 +0000

    modificações no módulo de busca

---
+++ deezer.py
@@ -0,0 +1,30 @@
+import requests
+
+def search_album_deezer(album_name, artist_name):
+    # Construir a URL da API do Deezer para pesquisa de álbuns
+    url = f"https://api.deezer.com/search/album/?q={album_name} {artist_name}&limit=1"
+
+    # Fazer a solicitação GET
+    response = requests.get(url)
+
+    # Verificar se a solicitação foi bem-sucedida (código 200)
+    if response.status_code == 200:
+        # Converter a resposta para JSON
+        data = response.json()
+
+        # Verificar se há resultados
+        if 'data' in data and data['data']:
+            album = data['data'][0]
+            print(f"Nome do álbum: {album['title']}")
+            print(f"Artista: {album['artist']['name']}")
+            print(f"Link do álbum no Deezer: {album['link']}")
+        else:
+            print("Álbum não encontrado no Deezer.")
+    else:
+        print(f"Falha na solicitação. Código de status: {response.status_code}")
+
+if __name__ == "__main__":
+    # Exemplo de pesquisa de álbum no Deezer
+    album_name = input("Digite o nome do álbum: ")
+    artist_name = input("Digite o nome do artista: ")
+    search_album_deezer(album_name, artist_name)
